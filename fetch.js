8caa7ae282e9f4c34db01cd5e18198ea4020ac5d
Rafa Sánchez Lucena <sanchez.lucena@gmail.com>
2022-08-03 18:21:13 +0200

    carga inicial y actualización de datos periódica

---
+++ fetch.js
@@ -0,0 +1,52 @@
+const div = document.querySelector(".container");
+
+const OPTIONS = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "46b09705b0msh419e77df9426930p19b7e4jsn2eb1d4271df5",
+    "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+  },
+};
+
+export const obtenerDatos = async (city) => {
+  try {
+    const response = await fetch(
+      `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${city}&days=3`,
+      OPTIONS
+    );
+    const data = await response.json();
+    
+    // Declaración de variables con los datos obtenidos.
+    let { current } = data;
+    let {
+      condition,
+      humidity,
+      temp_c,
+      wind_dir,
+      wind_kph,
+      precip_in,
+      pressure_mb,
+      feelslike_c,
+    } = current;
+    let { icon } = condition;
+
+    //  Pintamos los datos obtenidos en pantalla.
+    div.innerHTML = /*html*/ `<div class="weather">
+                      <div class="header">
+                      <span class="poblacion">${city}</span>
+                      <button  id="icono" onclick="changeCity()" class="icon"><span class="material-symbols-outlined"> settings </span></button>
+                      <br></div>
+                      <span><strong>Temp:</strong> ${temp_c}º,</span>
+                      <span><strong>Sensación:</strong> ${feelslike_c}º</span><br>
+                      <span><strong>Presión:</strong> ${pressure_mb}Pa,</span>
+                      <span><strong>Humedad:</strong> ${humidity}%</span><br>
+                      <span><strong>Viento:</strong> ${wind_kph}Km/h ${wind_dir}</span><br>
+                      <span><strong>Lluvia hoy:</strong> ${precip_in}mm</span><br>
+                      <img src="${icon}"/>                   
+                    </div>
+    `;
+  } catch (error) {
+    alert("La búsqueda no dio resultado.");
+    console.log(error);
+  }
+};
--- main.js
+++ main.js
@@ -1,68 +1,33 @@
 import "./style.css";
+import { obtenerDatos } from "./fetch";
 
-const div = document.querySelector(".container");
+const btn = document.querySelector(".modal__close");
+const modal = document.querySelector(".modal");
+const input = document.querySelector(".modal__input");
 
-const OPTIONS = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "46b09705b0msh419e77df9426930p19b7e4jsn2eb1d4271df5",
-    "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
-  },
+// Carga de datos inicial con la población "Alcoi" por defecto.
+const cargaInicial = () => {
+  let ciudad = "Alcoi";
+  obtenerDatos(ciudad);
+  setInterval(obtenerDatos, 300000, ciudad);
 };
 
-// fetch("https://weatherapi-com.span.rapidapi.com/current.json?q=madrid", OPTIONS)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
+cargaInicial();
 
-const obtenerDatos = async (city) => {
-  try {
-    const response = await fetch(
-      `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${city}&days=3`,
-      OPTIONS
-    );
-    const data = await response.json();
-    // Declaración de variables con los datos obtenidos.
-    let { current } = data;
-    let {
-      condition,
-      humidity,
-      temp_c,
-      wind_dir,
-      wind_kph,
-      precip_in,
-      pressure_mb,
-      feelslike_c,
-    } = current;
-    let { icon } = condition;
-
-    div.innerHTML = /*html*/ `<div class="weather">
-                      <div class="header">
-                      <span class="poblacion">${city}</span>
-                      <button  id="icono" onclick="changeCity()" class="icon"><span class="material-symbols-outlined"> settings </span></button>
-                      <br></div>
-                      <span><strong>Temp:</strong> ${temp_c}º,</span>
-                      <span><strong>Sensación:</strong> ${feelslike_c}º</span><br>
-                      <span><strong>Presión:</strong> ${pressure_mb}Pa,</span>
-                      <span><strong>Humedad:</strong> ${humidity}%</span><br>
-                      <span><strong>Viento:</strong> ${wind_kph}Km/h ${wind_dir}</span><br>
-                      <span><strong>Lluvia hoy:</strong> ${precip_in}mm</span><br>
-                      <img src="${icon}"/>                   
-                    </div>
-    `;
-  } catch (error) {
-    alert("La búsqueda no dio resultado.");
-    console.log(error);
-  }
-};
-
-obtenerDatos("Alcoi");
-
+// Mostramos el modal para elegir una nueva población.
 window.changeCity = () => {
-  const nombre = prompt("Elige otra población");
-  if (nombre != "" && nombre != null) {
-    obtenerDatos(nombre);
-  } else {
-    alert("Campo obligatorio.");
-  }
+  modal.classList.add("modal--show");
 };
+
+// Carga de datos con el nuevo nombre de la población y cierre del modal.
+btn.addEventListener("click", () => {
+  let valor = input.value;
+  if (valor != "" && valor != null) {
+    obtenerDatos(valor);
+    setInterval(obtenerDatos, 300000, valor)
+  } else {
+    alert("Campo obligatorio");
+  }
+  modal.classList.remove("modal--show");
+  input.value = "";
+});
